38a28a8bacbf29b4be1d00e3d4baf6a431291088
Seif <seifeldinomar11@gmail.com>
2025-04-13 22:52:21 +0200

    Starting the Preprocessing Phase

---
+++ scripts/preprocessing.py
@@ -0,0 +1,12 @@
+def preprocess(df):
+    """
+    Preprocess the input DataFrame by removing unnecessary columns and renaming others.
+
+    Args:
+        df (pd.DataFrame): The input DataFrame to preprocess.
+
+    Returns:
+        pd.DataFrame: The preprocessed DataFrame.
+    """
+    
+    return df
